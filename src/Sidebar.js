cf45f5c62fae86cbc6af1874ca908c2ea821e21b
Daniel Balloi <danielballoi1995@outlook.it>
2022-03-15 12:06:20 +0100

    correzione nome

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,7 +13,7 @@ import PeopleIcon from '@mui/icons-material/People';
 function Sidebar () {
 return (
 <div className='sidebar'>
-<SidebarRow src = 'https://media-exp1.licdn.com/dms/image/C4E03AQF7IbShaPnOLA/profile-displayphoto-shrink_200_200/0/1637539822456?e=1651708800&v=beta&t=dQE__NGIg_BUnSS77jmAPJMUpGlRAWSv90UZWQrSgI4' title='Dani Balloi'/>
+<SidebarRow src = 'https://media-exp1.licdn.com/dms/image/C4E03AQF7IbShaPnOLA/profile-displayphoto-shrink_200_200/0/1637539822456?e=1651708800&v=beta&t=dQE__NGIg_BUnSS77jmAPJMUpGlRAWSv90UZWQrSgI4' title='Daniel Balloi'/>
 <SidebarRow Icon={LocalHospitalIcon} title ='Covid-19 info'/>
 <SidebarRow Icon={EmojiEmotionsIcon}title='Pagine'/>
 <SidebarRow Icon={PeopleIcon} title='Amici'/>
